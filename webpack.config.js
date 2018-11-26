d0bddc1f4508d94a2efa017bfe106aa6048645b3
Chris Anderson <chris.scott.anders@gmail.com>
2018-11-26 02:16:13 -0500

    Get client side working
    * Restructured project
    * Added webpack for client side
    * Changed `npm dev` to rebuild and restart server on code changes
    * Implemented tutorial to authenticate

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/client/app.js',
+    // watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: [
+                    /node_modules(\/|\\)(?!(@feathersjs|debug))/
+                ],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    node: {
+        net: 'empty',
+        tls: 'empty',
+        fs: 'empty'
+    }
+};
