3e52e670d9c1506869b5de5097d8976a96ad2e40
Chris Anderson <chris.scott.anders@gmail.com>
2018-11-27 19:34:11 -0500

    Bold brand

--- src/client/components/Navbar.vue
+++ src/client/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">UF-STD</a>
+        <a class="navbar-brand mb-0 h1" href="#">UF-STD</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
