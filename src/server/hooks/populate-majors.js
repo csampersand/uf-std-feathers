3c1f387d502e75579edac7bf054eb5d03b8edcb4
rahulpatel1994 <rahuldpatel@ufl.edu>
2018-11-30 19:33:13 -0500

    Add some commments

--- src/server/hooks/populate-majors.js
+++ src/server/hooks/populate-majors.js
@@ -6,12 +6,10 @@ module.exports = function (options = {}) {
     return async context => {
         const { app, method, result, params } = context;
 
-        // Make sure that we always have a list of posts either by wrapping
-        // a single post into an array or by getting the `data` from the `find` method's result
+        //Gets all users within the schema
         const users = method === 'find' ? result.data : [ result ];
 
-        // Asynchronously get user object from each post's `author`
-        // and add it to the post
+        // Set the user's major based on the id within the majors table
         await Promise.all(users.map(async user => {
             // Also pass the original `params` to the service call
             // so that it has the same information available (e.g. who is requesting it)
