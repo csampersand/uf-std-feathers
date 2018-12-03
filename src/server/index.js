63c2371400171b1368f5082c4b4a3f2c8bda1ebc
carlosA-A <c.avogadro@ufl.edu>
2018-12-02 22:27:30 -0500

    test

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 const logger = require('./logger');
 const app = require('./app');
-const port = app.get('port');
+const port = process.env.PORT || 3000;
 const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
@@ -9,5 +9,5 @@ process.on('unhandledRejection', (reason, p) =>
 );
 
 server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+  logger.info('Feathers application started on http://%s:%d', window.location.href, port)
 );
