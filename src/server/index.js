f763d7056dcc14d9a06b601ea2dceec92a932f11
carlosA-A <c.avogadro@ufl.edu>
2018-12-03 00:19:37 -0500

    prod fix

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 const logger = require('./logger');
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port = app.get('port');
 const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
