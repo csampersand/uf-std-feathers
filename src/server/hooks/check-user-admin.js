9e2c084e96454d1bef4ce6ed2426afaafbd1d449
Carlos <c.avogadro@ufl.edu>
2018-12-05 09:58:54 -0500

    Fixed delete post

--- src/server/hooks/check-user-admin.js
+++ src/server/hooks/check-user-admin.js
@@ -3,7 +3,7 @@
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     return async context => {
-        const { data } = context;
+        const {app, data } = context;
 
         // The authenticated user
         const user = context.params.user;
@@ -11,12 +11,13 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
 
         // When generating dummy data through script allow the database to be cleared
         if(context.params.provider != undefined){
+            const post = await app.service('posts').get(context.id);
+
             //Only admins can remove a post
-            if(!user.isAdmin){
+            if(!(user.isAdmin || String(post.author._id) == String(user._id))){
                 throw new Error('Must be admin to remove a post');
             }
         }
-
         // Best practice: hooks should always return the context
         return context;
     };
