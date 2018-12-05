be33883d2d6b2a0dc4721917e9e181abce196cfe
Chris Anderson <chris.scott.anders@gmail.com>
2018-12-05 06:19:47 -0500

    Revert "Implement Edit Post"
    This reverts commit e5e5cf77e2c1d1cf9c3dc99c2364b65d23e48c83.

--- src/client/components/Post.vue
+++ src/client/components/Post.vue
@@ -1,73 +1,24 @@
 <script>
-    import * as services from '../services'
-    import swal from 'sweetalert';
-
-
     export default {
-        props: ['user', 'post','major'],
-        data() {
-            return {
-                editing: false
-            }
-        },
-        methods: {
-            editPost() {
-                services.postService.patch(this.post._id, this.post).then(() => {
-                    swal("Edited your post!", "Your post was edited successfully.", "success", {
-                        buttons: false,
-                        timer: 2000
-                    });
-                    this.editing = false;
-                }).catch(() => {
-                    swal("Uh oh!", "There was an error editing your post. Please try again later.", "error", {
-                        buttons: false,
-                        timer: 2000
-                    });
-                })
-            }
-            deletePost() {
-                services.postService.patch(this.post._id, this.post).then(() => {
-                    swal("Edited your post!", "Your post was edited successfully.", "success", {
-                        buttons: false,
-                        timer: 2000
-                    });
-                    this.editing = false;
-                }).catch(() => {
-                    swal("Uh oh!", "There was an error editing your post. Please try again later.", "error", {
-                        buttons: false,
-                        timer: 2000
-                    });
-                })
-            }
-        }
+        props: ['user', 'post','major']
     }
 </script>
 
 <template>
-<div>
   <div class="jumbotron" style="padding-top:10px; padding-bottom:10px;">
-    <h4 v-if="!editing" class="h2">{{ post.title }}</h4>
-    <input v-else class="form-control" v-model="post.title"></input>
-    <i>posted by </i> <router-link :to="{ name:'blog', params: { userId: post.author._id} }" href="#"><b>{{ post.author.fname }} {{ post.author.lname }}</b></router-link>
+    <h4 class="h2">{{ post.title }}</h4>
+    <router-link class="dropdown-item" :to="{ name:'blog', params: { userId: post.author._id} }" href="#"><i>posted by </i><b>{{ post.author.fname }} {{ post.author.lname }}</b></router-link>
     <hr style="width: 90%;">
 
-    <p v-if="!editing" class="blog-text">
+    <p class="blog-text">
       {{ post.body }}
     </p>
-    <textarea v-else class="form-control" id="editPostBody" rows="5" v-model="post.body"></textarea>
-
     <div class="clearfix">
-      <div v-if="!editing" style="float:right;">
-        <button v-if="user._id == post.author._id || user.isAdmin" type="button" v-on:click="editing = true" class="btn btn-info btn-lg">Edit</button>
+      <div style="float:right;">
+        <button v-if="user._id == post.author._id || user.isAdmin" type="button" class="btn btn-info btn-lg">Edit</button>
         <button v-if="user._id == post.author._id || user.isAdmin" type="button" class="btn btn-danger btn-lg">Delete</button>
         <button type="button" class="btn btn-warning btn-lg">Report</button>
       </div>
-      <div v-else style="float:right;">
-        <button v-on:click="editing=false" class="btn btn-error btn-lg">Cancel</button>
-        <button v-on:click="editPost" type="submit" class="btn btn-primary btn-lg">Save Changes</button>
-      </div>
-
     </div>
   </div>
-</div>
 </template>
