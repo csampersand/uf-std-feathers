2486776aad172179d231ce232470a72dc7dbe8b7
Chris Anderson <chris.scott.anders@gmail.com>
2018-12-05 06:37:18 -0500

    Refix UserAccounts (fuck you git)

--- src/client/components/UserAccount.vue
+++ src/client/components/UserAccount.vue
@@ -2,6 +2,8 @@
 <script>
 import * as services from '../services'
 
+import Post from './Post.vue'
+
     export default {
         props: ['user'],
         data() {
@@ -51,6 +53,9 @@ import * as services from '../services'
           x.innerHTML = "COMMENT";
         }
       }
+    },
+    components: {
+        Post
     }
 }
 
@@ -58,12 +63,12 @@ import * as services from '../services'
 </script>
 
 <template>
-<div v-if="author">
-    <div v-if="author.major" style="float:left; padding-top: 15px; padding-left: 15px;">
+<div v-if="author" class="row">
+    <div v-if="author.major" class="col">
 
-      <div style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); max-width: 300px; margin: auto; text-align: center; float: left;" >
+      <div style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); max-width: 300px; margin: 10px; text-align: center;" >
 
-        <h1>{{author.fname}}</h1>
+        <h1>{{author.fname}} {{author.lname}}</h1>
         <p class="user-account-title">{{author.major.majorName}}, {{author.gradYear}}</p>
         <p style="text-align:left; padding-left:10px; padding-right:10px;">
           <b>Bio: </b>
@@ -73,62 +78,22 @@ import * as services from '../services'
         <a class="user-account-a" href="http://linkedin.com"><i class="fa fa-linkedin" style="font-size:40px"></i></a>
         <a class="user-account-a" href="http://facebook.com"><i class="fa fa-facebook" style="font-size:40px"></i></a>
 
-        <p><button type="button" class="btn btn-danger" style="width:80%;"><b>Report User</b></button></p>
+        <p><button type="button" class="btn btn-danger" style="width:80%; margin-bottom:20px;"><b>Report User</b></button></p>
 
       </div>
     </div>
 
     <!-- BLOG POST -->
-    <dir v-if="posts">
-        <div style="float: right; width: 65%;">
-            <h1 class="display-3" style="padding-bottom:20px;"><b>{{author.fname}}'s Blog</b></h1>
-        </div>
-
-  <div style="float: right; width: 65%;"  v-for="post in posts" :key="post._id" v-if="post.author._id == author._id">
-
-      <div class="jumbotron" style="padding-top:10px; padding-bottom:10px;">
-        <h2 class="display-4">{{post.title}}</h2>
-        <h6><i>posted by </i><b>{{author.fname}}</b></h6>
-        <hr style="width: 90%;">
-
-        <p class="blog-text">
-          {{post.body}}
-        </p>
-        <div class="clearfix">
-          <button type="button" class="btn btn-primary btn-lg" >COMMENT SECTION</button>
-          <div style="float:right;">
-            <button type="button" class="btn btn-info btn-lg">EDIT</button>
-            <button type="button" class="btn btn-danger btn-lg">DELETE</button>
-            <button type="button" class="btn btn-warning btn-lg">REPORT POST</button>
-          </div>
-        </div>
-      </div>
-
-      <div id="my-reply" class="collapse">
-        <div class="jumbotron" style="width:90%;margin: auto;position: relative;float: inherit;padding-top:10px; padding-bottom:10px;">
-          <h6 class="blog-text"><i>comment posted by </i><b>Username</b></h6>
-          <div class="form-group">
-            <textarea class="form-control" rows="3" maxlength="1000" placeholder="say something, I'm giving up on you..." required></textarea>
-              <div style="padding:5px;"></div>
-              <button type="submit" class="btn btn-primary">POST</button>
-          </div>
-        </div>
-        <div style="padding:17px;"></div>
-      </div>
-
-      <div id="comment-section" class="collapse">
-        <div class="jumbotron" style="width:90%;margin: auto;position: relative;float: inherit;padding-top:10px; padding-bottom:10px;">
-          <h6 class="blog-text"><i>comment posted by </i><b>Username</b></h6>
-          <p class="blog-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-          <button type="submit" class="btn btn-danger">DELETE</button>
-        </div>
-      </div>
+    <div class="col-6" v-if="posts && user && author">
+        <post
+            v-for="post in posts"
+            v-if="author._id == post.author._id"
+            :key="post._id"
+            v-bind:post="post"
+            v-bind:user="user">
+        </post>
     </div>
-    </dir>
+    <div class="col"></div>
 
 
 </div>
@@ -147,56 +112,3 @@ import * as services from '../services'
     font-size: 22px;
     color: black;
 }
-
-/* .user-account-body {font-family: Arial, Helvetica, sans-serif;
-  padding-top: 5px;
-  padding-left: 15px;
-  float: left;
-  position: fixed;
-}
-
-.user-account-card {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    max-width: 300px;
-    margin: auto;
-    text-align: center;
-    float: left;
-}
-
-.user-account-button {
-    border: none;
-    outline: 0;
-    display: inline-block;
-    padding: 8px;
-    color: white;
-    background-color: grey;
-    text-align: center;
-    cursor: pointer;
-    width: 100%;
-    font-size: 18px;
-}
-
-.user-account-button:hover, a:hover {
-    opacity: 0.7;
-}
-
-.user-account-button-report {
-    border: none;
-    float: left;
-    outline: 0;
-    display: inline-block;
-    padding: 8px;
-    color: white;
-    background-color: darkgray;
-    text-align: center;
-    cursor: pointer;
-    width: 20%;
-    font-size: 18px;
-}
-
-.user-account-button-report:hover {
-    opacity: 0.7;
-    background-color: darkred;
-} */
-
-</style>
