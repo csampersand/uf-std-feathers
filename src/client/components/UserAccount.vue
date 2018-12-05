cbd01191f2c9887d8979aeea282773b8546c6cec
Carlos <c.avogadro@ufl.edu>
2018-12-05 00:26:51 -0500

    working on the user account page

--- src/client/components/UserAccount.vue
+++ src/client/components/UserAccount.vue
@@ -1,13 +1,16 @@
 <script>
-          function swapText() {
+    export default {
+        props: ['user'],
+        methods:{
+        swapText() {
         var x = document.getElementById("myComments");
         if (x.innerHTML === "COMMENT SECTION") {
           x.innerHTML = "COLLAPSE";
         } else {
           x.innerHTML = "COMMENT SECTION";
         }
-      };
-            function replyComment() {
+      },
+        replyComment() {
         var x = document.getElementById("myReply");
         if (x.innerHTML === "COMMENT") {
           x.innerHTML = "CANCEL";
@@ -15,23 +18,24 @@
           x.innerHTML = "COMMENT";
         }
       }
+    }
+}
+
+
 </script>
 
 <template>
-        <div class="user-account-body">
-      <img class="user-account-img" src="./img/blank-profile-img.png">
+<div v-if="user">
+        <div class="user-account-body" >
+      <!-- <img class="user-account-img" src="./img/blank-profile-img.png"> -->
 
-      <div class="user-account-card">
+      <div class="user-account-card" >
 
-        <h1>John Doe</h1>
+        <h1>{{user.fname}}</h1>
         <p class="user-account-title">Major, Year</p>
         <p style="text-align:left; padding-left:10px; padding-right:10px;">
           <b>Bio: </b>
-        This is the student from Harvard University. This is the student from Harvard University.
-        This is the student from Harvard University. This is the student from Harvard University.
-        This is the student from Harvard University. This is the student from Harvard University.
-        This is the student from Harvard University. This is the student from Harvard University. </p>
-
+            {{user.bio}}
         <a class="user-account-a" href="http://instagram.com"><i class="fa fa-instagram" style="font-size:40px"></i></a>
         <a class="user-account-a" href="http://twitter.com"><i class="fa fa-twitter" style="font-size:40px"></i></a>
         <a class="user-account-a" href="http://linkedin.com"><i class="fa fa-linkedin" style="font-size:40px"></i></a>
@@ -43,8 +47,8 @@
     </div>
 
     <!-- BLOG POST -->
-    <div class="box-frame">
-      <h1 class="display-3" style="padding-bottom:20px;"><b>Username's Blog</b></h1>
+    <div class="box-frame" style="position: absolute;right:0;top:0">
+      <h1 class="display-3" style="padding-bottom:20px;"><b>{{user.fname}}'s Blog</b></h1>
 
       <div class="jumbotron" style="padding-top:10px; padding-bottom:10px;">
         <h2 class="display-4">Toppic Of Discussion</h2>
@@ -90,4 +94,81 @@
       </div>
     </div>
 
+</div>
+
 </template>
+
+<style>
+.user-account-body {font-family: Arial, Helvetica, sans-serif;
+  padding-top: 5px;
+  padding-left: 15px;
+  float: left;
+  position: fixed;
+}
+
+.user-account-img {
+  float: center;
+  max-width: 300px;
+  border-radius: 50%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  padding-bottom: 10px;
+}
+
+.user-account-card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    max-width: 350px;
+    margin: auto;
+    text-align: center;
+}
+
+.user-account-title {
+    color: grey;
+    font-size: 18px;
+}
+
+.user-account-button {
+    border: none;
+    outline: 0;
+    display: inline-block;
+    padding: 8px;
+    color: white;
+    background-color: grey;
+    text-align: center;
+    cursor: pointer;
+    width: 100%;
+    font-size: 18px;
+}
+
+.user-account-a {
+    text-decoration: none;
+    font-size: 22px;
+    color: black;
+}
+
+.user-account-button:hover, a:hover {
+    opacity: 0.7;
+}
+
+.user-account-button-report {
+    border: none;
+    float: left;
+    outline: 0;
+    display: inline-block;
+    padding: 8px;
+    color: white;
+    background-color: darkgray;
+    text-align: center;
+    cursor: pointer;
+    width: 20%;
+    font-size: 18px;
+}
+
+.user-account-button-report:hover {
+    opacity: 0.7;
+    background-color: darkred;
+}
+
+</style>
